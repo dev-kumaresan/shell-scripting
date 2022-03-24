a3d9bc289dae752256cb21c008e15a1e162b24a2
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-03-24 09:46:12 +0530

    Read-User-Input

--- Read-User-Input/script1.sh
+++ Read-User-Input/script1.sh
@@ -1,5 +1,13 @@
 # Read User Input
 
+# Read a single variable
+
 echo "Enter Name"
 read name
 echo "The name is "$name
+
+# Read Multiple Variables 
+
+echo "Enter Names : "
+read name1 name2 name3
+echo "The Names are : "$name1,$name2,$name3
