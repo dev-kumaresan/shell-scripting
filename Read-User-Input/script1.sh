71f2c76436120bfc0279784daa9034d7fc42016a
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-03-24 10:14:26 +0530

    Prompt-input-same-line

--- Read-User-Input/script1.sh
+++ Read-User-Input/script1.sh
@@ -11,3 +11,11 @@ echo "The name is "$name
 echo "Enter Names : "
 read name1 name2 name3
 echo "The Names are : "$name1,$name2,$name3
+
+# prompt input cursor in same line
+
+read -p 'Enter Name : ' Name
+echo "Name : "$Name
+
+# prompt credentials as hidden 
+# Example user password
