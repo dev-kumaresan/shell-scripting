4ed6740b0cf1b8a0c2dea85cdf513999271b4580
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-18 20:12:31 +0530

    updated

--- File-test-Operators/index.sh
+++ File-test-Operators/index.sh
@@ -1,17 +1,19 @@
 # File test Operators
+
 # echo  "Enter the name of the file : \c"
 # read filename
 # if [ -e $filename ]
-# then
+# then                                                                  ## check the particular file is found or not.
 # echo "$filename found"
 # else
 # echo "$filename not found"
 # fi
+
 echo  "Enter the name of the file : \c"
 read filename
 if [ -s $filename ]
 then
-echo "$filename not empty"
+echo "$filename not empty"                                              ## check the particular file have character(s) or empty file.
 else
 echo "$filename empty"
 fi
