8ea19799608b3eb4e7b3822c79366d02d2a05c6a
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-18 20:30:03 +0530

    updated

--- Logical-operators/AND.sh
+++ Logical-operators/AND.sh
@@ -2,11 +2,15 @@
 
 # read -p "Enter Age : " age
 # if [ "$age" -gt 18 ] && [ "$age" -lt 30 ]
-# then
+# then                                                          
 # echo "vote"
 # else
 # echo "not vote"
 # fi
+
+
+    # (or)
+
 
 read -p "Enter Age : " age
 if [ "$age" -gt 18  -a "$age" -lt 30 ]
